62fc8c1a938890dc6759889654dc66a2af6e94c1
systemad <dannesuke@gmail.com>
2021-03-11 20:40:52 +0100

    Re-added resetprop

--- device.mk
+++ device.mk
@@ -56,7 +56,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+    fastbootd \
+    resetprop
 
 # qcom decryption
 PRODUCT_PACKAGES_ENG += \
