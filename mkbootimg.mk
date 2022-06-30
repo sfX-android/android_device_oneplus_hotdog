4adaab24fccaf94773c3394365d2465cb7c68932
steadfasterX <steadfasterX@users.noreply.github.com>
2022-06-30 11:32:11 +0200

    Update mkbootimg.mk

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -4,7 +4,7 @@ LOCAL_PATH := $(call my-dir)
 # path can NOT be changed (w/o changing build/make, too) !
 BOARD_PREBUILT_RECOVERY := /tmp/recovery/IMAGES/recovery.img
 
-ROOT_BOOT_BIN := $(OUT_DIR)/.magisk/root_boot.sh
+ROOT_BOOT_BIN := $(OUT_DIR)/.magisk/boot_patch.sh
 
 BOARD_CUSTOM_BOOTIMG := true
 
@@ -14,15 +14,15 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(AVBTOOL) $(INTERNAL_BOOTIMAGE_FILE
 	$(call pretty,"Target boot image: $@")
 	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	@echo "++++  PRE-ROOTing BOOT image!!!  ++++"
-	MAGISK_TARGET_ARCH=arm64 $(ROOT_BOOT_BIN) $$PWD/$@
+	@/bin/bash $(ROOT_BOOT_BIN) $$PWD/$@
 	$(hide) $(call assert-max-image-size,$@,$(call get-hash-image-max-size,$(BOARD_BOOTIMAGE_PARTITION_SIZE)))
 	@echo "++++  Add hash footer to BOOT image  ++++"
 	$(hide) $(AVBTOOL) add_hash_footer \
-          --image $(OUT_DIR)/.magisk/IMAGES/boot.img \
+          --image $(OUT_DIR)/.magisk/new-boot.img \
           --partition_size $(BOARD_BOOTIMAGE_PARTITION_SIZE) \
           --partition_name boot $(INTERNAL_AVB_BOOT_SIGNING_ARGS) \
           $(BOARD_AVB_BOOT_ADD_HASH_FOOTER_ARGS)
-	@cp -v $(OUT_DIR)/.magisk/IMAGES/boot.img  $(PRODUCT_OUT)/boot.img
+	@cp -v $(OUT_DIR)/.magisk/new-boot.img  $(PRODUCT_OUT)/boot.img
 else
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(AVBTOOL) $(INTERNAL_BOOTIMAGE_FILES) $(BOARD_AVB_BOOT_KEY_PATH)
 	$(call pretty,"Target boot image: $@")
