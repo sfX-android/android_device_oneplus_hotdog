a401b92b6afa049090279b4000f93c39c979dc50
steadfasterX <steadfasterX@users.noreply.github.com>
2022-09-08 11:57:29 +0200

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-############# /e/ section ###############
-export EOS_DEVICE=hotdog
-#export EOS_BRANCH_NAME="v1-q"
-#export EOS_OTA_CLEARTEXT=true
-export EOS_USE_CCACHE=true
-export EOS_CCACHE_DIR=/ssd/ccache/jenkins/android-10.0
-export EOS_CCACHE_SIZE=14G
-export EOS_TMP_DIR=/ssd/tmp/eos
-export EOS_ZIP_DIR=/ssd/tmp/eos/zips
